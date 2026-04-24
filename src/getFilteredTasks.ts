ead1a6490f43aaba059545f92b33734f4dcc17b7
Natalia Trifonova <trifonova.dev@gmail.com>
2026-04-24 14:12:03 +0700

    two times train

--- src/getFilteredTasks.ts
+++ src/getFilteredTasks.ts
@@ -1,13 +1,16 @@
 import {FilterValues, Task} from "./App.tsx";
 
-export const getFilteredTasks = (tasks: Task[], filter: FilterValues) => {
+export const getFiltetedTasks = (tasks: Task[], filter: FilterValues) => {
+
     let filteredTasks = tasks
-    if (filter === 'active') {
+    if (filter === 'active'
+    ) {
         filteredTasks = tasks.filter(task => !task.isDone)
     }
     if (filter === 'completed') {
         filteredTasks = tasks.filter(task => task.isDone)
     }
     return filteredTasks
-
 }
+
+
